b0ac93b87f79c737982d319b35e28bcd81e6ee5c
OrchunP <100283947+OrchunP@users.noreply.github.com>
2022-08-01 20:04:13 +0300

    Add files via upload

---
+++ TicTacToe.py
@@ -0,0 +1,53 @@
+class ttt:
+    def __init__(self):
+        self.board = []
+    def c_board(self):
+        for i in range(3):
+            row = []
+            for j in range(3):
+                row.append("-")
+            self.board.append(row)
+    def s_board(self):
+        for i in self.board:
+            for j in i:
+                print(j,end="")
+            print()
+    def player(self):
+        return True
+    def player_change(self,player1):
+        if player1 == "X":
+            return True
+        else:
+            return False
+
+    def mark(self,row,clm,player):
+        self.board[row][clm] = player
+
+
+    def start(self):
+        self.c_board()
+        player1 = " "
+        if self.player() == True:
+            player1 = "X"
+        else:
+            player1 = "O"
+
+        while True:
+            print("Player {} Turn".format(player1))
+
+            self.s_board()
+
+            row, clm = list(
+                map(int,input("sayı:").split()))
+            print()
+
+            self.mark(row -1,clm -1,player1)
+            if self.player_change(player1):
+                player1 = "O"
+            else:
+                player1 = "X"
+
+q = ttt()
+q.start()
+
+
